264ba97cd80cf29a6297116dd25ab22eea816199
zbodnick <zacharyb@bhgroup.finance>
2021-09-20 12:21:39 -0400

    Added sphere colliders & XR direct interactable to each hand controller, set VR Cam clipping plane to 0.1 (prevents  player from being able to see inside models they  pickup and bring close to their face)

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+	public string nextScene;
+
+	public void LoadScene() {
+		SceneManager.LoadScene(nextScene);
+	}
+
+}
