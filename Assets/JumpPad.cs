f74c02e068ccc95fdfa7022c3ad3aa3b34ff7b3d
zbodnick <zacharyb@bhgroup.finance>
2021-10-20 01:12:52 -0400

    implemented jump pads

---
+++ Assets/JumpPad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour {
+
+	Vector3 direction; 
+	public float jumpForce = 50f;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        direction = transform.TransformDirection(Vector3.up * jumpForce);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+		
+		if (collision.gameObject.CompareTag("Player")) {
+			// Apply jump force
+			collision.gameObject.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
+		}
+	}
+
+}
